44c799cccca02b327f037c72bd0e0a21d52a4e37
Maryna Kamysheva <marina.stozhok@gmail.com>
2023-09-14 16:47:55 -0600

    feat: Initial setup.

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
+
+const App: FC = () => {
+  return (
+    <>
+      <StatusBar style="auto" />
+
+      <Text>Open up App.js to start working on your app!</Text>
+    </>
+  );
+};
+
+export default App;
